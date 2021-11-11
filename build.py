6efba4e903c4ec64ca606ed894e28e6c085fba78
Francesc Elies <francesc.elies@mbbm-ast.com>
2021-11-11 12:55:18 +0100

    cffi api mode

--- build.py
+++ build.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import subprocess
 
 def run(*args, **kwargs):
@@ -23,6 +24,43 @@ clang_flags = ("-std=c99",
                "-fno-optimize-sibling-calls",
                )
 with_asan = ("-shared-libsan", "-fsanitize=address")
+
+
+def build_cffi_api_mode(cffi_api_mode_c_file: Path):
+    r"""
+    Compiles a dll for cffi in API Mode
+
+    Tries to do the same as msvc but with clang as ffi.compile which makes the following calls
+       C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Tools\MSVC\14.29.30133\bin\HostX86\x64\cl.exe /c /nologo /Ox /W3 /GL /DNDEBUG /MD -Ic:\s\asan-cffi-min-reprex\venv\include -IC:\Python39\include -IC:\Python39\include -IC:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Tools\MSVC\14.29.30133\ATLMFC\include -IC:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Tools\MSVC\14.29.30133\include -IC:\Program Files (x86)\Windows Kits\NETFXSDK\4.8\include\um -IC:\Program Files (x86)\Windows Kits\10\include\10.0.19041.0\ucrt -IC:\Program Files (x86)\Windows Kits\10\include\10.0.19041.0\shared -IC:\Program Files (x86)\Windows Kits\10\include\10.0.19041.0\um -IC:\Program Files (x86)\Windows Kits\10\include\10.0.19041.0\winrt -IC:\Program Files (x86)\Windows Kits\10\include\10.0.19041.0\cppwinrt /Tccffi_example_dll.c /Fo.\Release\cffi_example_dll.obj
+       C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Tools\MSVC\14.29.30133\bin\HostX86\x64\link.exe /nologo /INCREMENTAL:NO /LTCG /DLL /MANIFEST:EMBED,ID=2 /MANIFESTUAC:NO /LIBPATH:c:\s\asan-cffi-min-reprex\venv\libs /LIBPATH:C:\Python39\libs /LIBPATH:C:\Python39 /LIBPATH:c:\s\asan-cffi-min-reprex\venv\PCbuild\amd64 /LIBPATH:C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Tools\MSVC\14.29.30133\ATLMFC\lib\x64 /LIBPATH:C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Tools\MSVC\14.29.30133\lib\x64 /LIBPATH:C:\Program Files (x86)\Windows Kits\NETFXSDK\4.8\lib\um\x64 /LIBPATH:C:\Program Files (x86)\Windows Kits\10\lib\10.0.19041.0\ucrt\x64 /LIBPATH:C:\Program Files (x86)\Windows Kits\10\lib\10.0.19041.0\um\x64 example.lib /EXPORT:PyInit_cffi_example_dll .\Release\cffi_example_dll.obj /OUT:.\cffi_example_dll.cp39-win_amd64.pyd /IMPLIB:.\Release\cffi_example_dll.cp39-win_amd64.lib
+
+    Docs https://cffi.readthedocs.io/en/latest/overview.html#api-mode-calling-the-c-standard-library
+    """
+    print("Clang version")
+    run("clang -v")
+
+    print(f"Compile object files")
+    object_file = cffi_api_mode_c_file.absolute().parent / f"{cffi_api_mode_c_file.stem}.obj"
+    run(" ".join(("clang",
+                    " ".join(clang_flags),
+                    " ".join(with_asan),
+                    "-I c_src",
+                    r"-I C:\Python39\include",
+                    f"-c {cffi_api_mode_c_file.absolute()}",
+                    f"-o {object_file}")))
+
+    dll_file = object_file.parent / f"{object_file.stem}.dll"
+
+    print(f"Compile dll")
+    run(" ".join(("clang",
+                    " ".join(clang_flags),
+                    " ".join(with_asan),
+                    "-Wl,/ignore:longsections",
+                    r"-I C:\Python39\libs"
+                    f"{object_file}",
+                    "-shared",
+                    f"-o {dll_file}")))
+    return dll_file
 
 
 def build():
--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import unittest
 from pathlib import Path
 
 import cffi
+from cffi.recompiler import make_c_source
 
 from build import build
 
@@ -14,17 +15,49 @@ def cleanCDEF(cdef: str):
   cdef = cdef.replace("__declspec( dllexport )", "")
   return cdef
 
-
-def test_example(asan=False):
+def load_ffi():
   ffiPath = file_directory / "c_src/ffi.i"
   ffi = cffi.FFI()
   clean_ffi = (file_directory / "c_src/clean_ffi.i")
   clean_ffi.write_text(cleanCDEF(ffiPath.read_text()))
-  ffi.cdef(clean_ffi.read_text(), pack=8)
+  ffi.cdef(clean_ffi.read_text())
+  return ffi
+
+
+def load_dll_cffi_api_mode(ffi):
+  # This C code needs to make the declarated functions, types
+  # and globals available, so it is often just the "#include".
+  preamble = """
+  // passed to the real C compiler,
+  // contains implementation of things declared in cdef()
+  #include "example.h"   // the C header of the library
+  """
+  cffi_api_mode_c_file = (file_directory / ("c_src/cffi_example_dll.c")).absolute()
+  updated_c_source = make_c_source(
+    ffi, module_name="cffi_example_dll", preamble=preamble,
+    target_c_file=cffi_api_mode_c_file, verbose=True)
+
+  from build import build_cffi_api_mode
+  dll_file = build_cffi_api_mode(cffi_api_mode_c_file)
+  return dll_file
+
+
+def test_example(asan_cffi_api_mode=False, no_asan=False, asan=False):
+  ffi = load_ffi()
+
+  dllPath = None
+
+  if asan_cffi_api_mode:
+    dllPath = load_dll_cffi_api_mode(ffi)
+
   if asan:
     dllPath = file_directory / "c_src/asanexample.dll"
-  else:
+
+  if no_asan:
     dllPath = file_directory / "c_src/example.dll"
+
+  assert dllPath
+
   print(f"dlopen {dllPath}")
   C = ffi.dlopen(str(dllPath.absolute()))
 
@@ -40,9 +73,11 @@ class Mytest(unittest.TestCase):
 if __name__ == '__main__':
   build()
 
+
   # This seems ok, but not sure
-  test_example()
+  test_example(no_asan=True)
   test_example(asan=True)
+  # test_example(asan_cffi_api_mode=True)
 
   # unittest explodes BOOM
   unittest.main()  # comment this line and AddressSanitizer will not complain
