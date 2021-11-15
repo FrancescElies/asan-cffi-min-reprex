7da07691b69e73baba79c82c6a97e7675f1395b6
Francesc Elies <francesc.elies@mbbm-ast.com>
2021-11-15 09:03:38 +0100

    refacor

--- build.py
+++ build.py
@@ -1,41 +1,137 @@
-from pathlib import Path
+import sys
+import os
+from cffi.recompiler import make_c_source
+import re
 import subprocess
+from contextlib import contextmanager, suppress
+from pathlib import Path
+from typing import Iterable, Optional
+
+import cffi
+
+repo = Path(__file__).absolute().parent
+
+@contextmanager
+def cwd(path):
+    oldpwd=os.getcwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(oldpwd)
+
 
 def run(*args, **kwargs):
     print(args[0])
     subprocess.run(*args, **kwargs, shell=True, check=True)
     print("\n\n")
 
-clang_flags = ("-std=c99",
-               "--target=x86_64-pc-windows-msvc",
-               "-fuse-ld=lld",
-               "-Wno-cast-align",
-               "-fcomment-block-commands=retval",
-               "-ferror-limit=200",
-               "-fmessage-length=0",
-               "-fno-short-enums",
-               "-ffunction-sections",
-               "-fdata-sections",
-               # debug symbos (-g)
-               "-g",
-               "-gdwarf-4",
-               "-O1",
-               "-fno-omit-frame-pointer",
-               "-fno-optimize-sibling-calls",
-               )
+
+def dumpbin(path: Path, imports=False, exports=False, symbols=False):
+    dumpbin_exe = (r"C:\Program Files (x86)\Microsoft Visual Studio\2019"
+                    r"\Professional\VC\Tools\MSVC\14.29.30133\bin\HostX86\x86"
+                    r"\dumpbin.exe" )
+
+    assert any((imports, exports, symbols))
+
+    if exports:
+        subprocess.run(rf'"{dumpbin_exe}" /exports "{path.absolute()}"')
+
+    if imports:
+        subprocess.run(rf'"{dumpbin_exe}" /imports "{path.absolute()}"')
+
+    if symbols:
+        subprocess.run(rf'"{dumpbin_exe}" /symbols "{path.absolute()}"')
+
+def llvm_nm(path: Path):
+    """ OVERVIEW: llvm symbol table dumper """
+    subprocess.run(rf'llvm-nm "{path.absolute()}"')
+
+
+clang_flags = (
+    "-std=c99",
+    "--target=x86_64-pc-windows-msvc",
+    "-fuse-ld=lld",
+    "-Wno-cast-align",
+    "-fcomment-block-commands=retval",
+    "-ferror-limit=200",
+    "-fmessage-length=0",
+    "-fno-short-enums",
+    "-ffunction-sections",
+    "-fdata-sections",
+    "-O1",
+    "-fno-omit-frame-pointer",
+    "-fno-optimize-sibling-calls",
+)
+
 with_asan = ("-shared-libsan", "-fsanitize=address")
 
 
-def build_cffi_api_mode(cffi_api_mode_c_file: Path):
+def cleanCDEF(cdef: str):
+    cdef = cdef.replace("__declspec( dllexport )", "")
+    return cdef
+
+
+def load_ffi():
+    ffiPath = repo / "c_src/ffi.i"
+    ffi = cffi.FFI()
+    clean_ffi = (repo / "c_src/clean_ffi.i")
+    clean_ffi.write_text(cleanCDEF(ffiPath.read_text()))
+    ffi.cdef(clean_ffi.read_text())
+    return ffi
+
+
+def build_pyd_msvc():
+    ffi = load_ffi()
+
+    with cwd(repo / "c_src"):
+        ffi.set_source(
+        module_name="_example",  # name of the output C extension
+        source="""
+        // passed to the real C compiler,
+        // contains implementation of things declared in cdef()
+        #include "example.h"   // the C header of the library
+        """,
+        sources=['example.c'],   # includes as additional sources
+        libraries=[]
+        )
+
+    ffi.compile(verbose=True)
+
+
+def build_pyd_clang_asan():
     r"""
-    Compiles a dll for cffi in API Mode
+    Compiles a pyd (dll) with clang and cffi in API Mode
 
     Tries to do the same as msvc but with clang as ffi.compile which makes the following calls
-       C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Tools\MSVC\14.29.30133\bin\HostX86\x64\cl.exe /c /nologo /Ox /W3 /GL /DNDEBUG /MD -Ic:\s\asan-cffi-min-reprex\venv\include -IC:\Python39\include -IC:\Python39\include -IC:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Tools\MSVC\14.29.30133\ATLMFC\include -IC:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Tools\MSVC\14.29.30133\include -IC:\Program Files (x86)\Windows Kits\NETFXSDK\4.8\include\um -IC:\Program Files (x86)\Windows Kits\10\include\10.0.19041.0\ucrt -IC:\Program Files (x86)\Windows Kits\10\include\10.0.19041.0\shared -IC:\Program Files (x86)\Windows Kits\10\include\10.0.19041.0\um -IC:\Program Files (x86)\Windows Kits\10\include\10.0.19041.0\winrt -IC:\Program Files (x86)\Windows Kits\10\include\10.0.19041.0\cppwinrt /Tccffi_example_dll.c /Fo.\Release\cffi_example_dll.obj
-       C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Tools\MSVC\14.29.30133\bin\HostX86\x64\link.exe /nologo /INCREMENTAL:NO /LTCG /DLL /MANIFEST:EMBED,ID=2 /MANIFESTUAC:NO /LIBPATH:c:\s\asan-cffi-min-reprex\venv\libs /LIBPATH:C:\Python39\libs /LIBPATH:C:\Python39 /LIBPATH:c:\s\asan-cffi-min-reprex\venv\PCbuild\amd64 /LIBPATH:C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Tools\MSVC\14.29.30133\ATLMFC\lib\x64 /LIBPATH:C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Tools\MSVC\14.29.30133\lib\x64 /LIBPATH:C:\Program Files (x86)\Windows Kits\NETFXSDK\4.8\lib\um\x64 /LIBPATH:C:\Program Files (x86)\Windows Kits\10\lib\10.0.19041.0\ucrt\x64 /LIBPATH:C:\Program Files (x86)\Windows Kits\10\lib\10.0.19041.0\um\x64 example.lib /EXPORT:PyInit_cffi_example_dll .\Release\cffi_example_dll.obj /OUT:.\cffi_example_dll.cp39-win_amd64.pyd /IMPLIB:.\Release\cffi_example_dll.cp39-win_amd64.lib
+
+       C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Tools\MSVC\14.29.30133\bin\HostX86\x64\cl.exe /c /nologo /Ox /W3 /GL /DNDEBUG /MD -Ic:\s\asan-cffi-min-reprex\venv\include -IC:\Python39\include -IC:\Python39\include -IC:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Tools\MSVC\14.29.30133\ATLMFC\include -IC:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Tools\MSVC\14.29.30133\include -IC:\Program Files (x86)\Windows Kits\NETFXSDK\4.8\include\um -IC:\Program Files (x86)\Windows Kits\10\include\10.0.19041.0\ucrt -IC:\Program Files (x86)\Windows Kits\10\include\10.0.19041.0\shared -IC:\Program Files (x86)\Windows Kits\10\include\10.0.19041.0\um -IC:\Program Files (x86)\Windows Kits\10\include\10.0.19041.0\winrt -IC:\Program Files (x86)\Windows Kits\10\include\10.0.19041.0\cppwinrt /Tccffi_example_dll.c /Fo.\Release\example_dll.obj
+
+       C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Tools\MSVC\14.29.30133\bin\HostX86\x64\link.exe /nologo /INCREMENTAL:NO /LTCG /DLL /MANIFEST:EMBED,ID=2 /MANIFESTUAC:NO /LIBPATH:c:\s\asan-cffi-min-reprex\venv\libs /LIBPATH:C:\Python39\libs /LIBPATH:C:\Python39 /LIBPATH:c:\s\asan-cffi-min-reprex\venv\PCbuild\amd64 /LIBPATH:C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Tools\MSVC\14.29.30133\ATLMFC\lib\x64 /LIBPATH:C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Tools\MSVC\14.29.30133\lib\x64 /LIBPATH:C:\Program Files (x86)\Windows Kits\NETFXSDK\4.8\lib\um\x64 /LIBPATH:C:\Program Files (x86)\Windows Kits\10\lib\10.0.19041.0\ucrt\x64 /LIBPATH:C:\Program Files (x86)\Windows Kits\10\lib\10.0.19041.0\um\x64 example.lib /EXPORT:PyInit_cffi_example_dll .\Release\example_dll.obj /OUT:.\example_dll.cp39-win_amd64.pyd /IMPLIB:.\Release\example_dll.cp39-win_amd64.lib
 
     Docs https://cffi.readthedocs.io/en/latest/overview.html#api-mode-calling-the-c-standard-library
     """
+
+    ffi = load_ffi()
+    out_folder = repo / "c_src/pyd_clang_asan"
+    with suppress(FileExistsError):
+        out_folder.mkdir()
+
+    module_name = "cffi_wrap"
+    cffi_api_mode_c_file = (repo / (f"{out_folder}/{module_name}.c")).absolute()
+
+    target_c_file = (repo / (f"{out_folder}/{module_name}.c")).absolute()
+    updated_c_source = make_c_source(
+        ffi, module_name=module_name,
+        preamble="""
+        // This C code needs to make the declarated functions, types
+        // and globals available, so it is often just the "#include".
+        // passed to the real C compiler,
+        // contains implementation of things declared in cdef()
+        #include "example.h"   // the C header of the library
+        """,
+        target_c_file=target_c_file, verbose=True)
+
     print("Clang version")
     run("clang -v")
 
@@ -46,52 +142,78 @@ def build_cffi_api_mode(cffi_api_mode_c_file: Path):
                     " ".join(with_asan),
                     "-I c_src",
                     r"-I C:\Python39\include",
-                    f"-c {cffi_api_mode_c_file.absolute()}",
-                    f"-o {object_file}")))
+                    "-fvisibility=default",
+                    f"-c example.c",
+                    f"-o {out_folder}/example.obj")), cwd=repo / "c_src")
 
-    dll_file = object_file.parent / f"{object_file.stem}.dll"
-
-    print(f"Compile dll")
     run(" ".join(("clang",
                     " ".join(clang_flags),
                     " ".join(with_asan),
-                    "-Wl,/ignore:longsections",
-                    r"-I C:\Python39\libs"
+                    f'-I {repo / "c_src"}',
+                    r"-I C:\Python39\include",
+                    "-fvisibility=default",
+                    f"-c {cffi_api_mode_c_file.absolute()}",
+                    f"-o {object_file}")), cwd=repo / "c_src")
+
+
+    # NOTE: hardcoded python and plaltform
+    # dll_file = object_file.parent / f"{object_file.stem}.cp39-win_amd64.pyd"
+    dll_file = object_file.parent / f"{object_file.stem}.pyd"
+
+    print(f"Compile dll")
+
+
+    run(" ".join(("clang",
+                    " ".join(clang_flags),
+                    " ".join(with_asan),
+                    # f"-Wl,/ignore:longsections,/export:PyInit_{object_file.stem}",  # :(
+                    f"-Wl,/ignore:longsections,/DEF:{object_file.stem}.def",  # :(
+                    r"-I C:\Python39\libs",
+                    r"-L C:\Python39\libs",
                     f"{object_file}",
+                    f"example.obj",
                     "-shared",
-                    f"-o {dll_file}")))
+                    "-v",
+                    f"-o {dll_file}")), cwd=out_folder)
+
     return dll_file
 
 
-def build():
+def build_dll_clang():
+    _build(out_folder=repo / "c_src/dll_clang")
 
-    print("Clean previous compilation")
-    # run("git clean -fdx c_src")
 
-    print("Clang version")
-    run("clang -v")
+def build_dll_clang_asan():
+    _build(out_folder=repo / "c_src/dll_clang_asan", extra_flags=with_asan)
 
-    print("Compile ffi")
+
+def _build(out_folder: Path,
+           extra_flags: Optional[Iterable[str]] = None):
+
+    extra_flags = extra_flags or ()
+
     run(" clang -I c_src -E c_src/example.h > c_src/ffi.i ")
 
-    for (prefix, extra_flags) in zip(("asan", ""), (with_asan, ())):
-        print(f"Compile object files {prefix}")
-        run(" ".join(("clang",
-                      " ".join(clang_flags),
-                      " ".join(extra_flags),
-                      "-I c_src",
-                      "-c c_src/example.c",
-                      f"-o c_src/{prefix}example.o")))
+    with suppress(FileExistsError):
+        out_folder.mkdir()
 
-        print(f"Compile dll {prefix}")
-        run(" ".join(("clang",
-                      " ".join(clang_flags),
-                      " ".join(extra_flags),
-                      "-Wl,/ignore:longsections",
-                      f"c_src/{prefix}example.o",
-                      "-shared",
-                      f"-o c_src/{prefix}example.dll")))
+    run(" ".join(("clang",
+                    " ".join(clang_flags),
+                    " ".join(extra_flags),
+                    "-I c_src",
+                    "-c c_src/example.c",
+                    f"-o {out_folder}/example.o")))
+
+    run(" ".join(("clang",
+                    " ".join(clang_flags),
+                    " ".join(extra_flags),
+                    "-Wl,/ignore:longsections",
+                    f"{out_folder}/example.o",
+                    "-shared",
+                    f"-o {out_folder}/example.dll")))
 
 
 if __name__ == '__main__':
-    build()
+    build_dll_clang()
+    build_dll_clang_asan()
+    build_pyd_clang_asan()
--- test.py
+++ test.py
@@ -1,67 +1,57 @@
-import re
 import unittest
+import os
 from pathlib import Path
 
-import cffi
-from cffi.recompiler import make_c_source
 
-from build import build
+from build import build_dll_clang, build_dll_clang_asan, load_ffi, dumpbin
 
+__file__ = "c:/s/asan-cffi-min-reprex/test.py"
 file_path = Path(__file__).absolute()
-file_directory = file_path.parent
+repo = file_path.parent
 
 
-def cleanCDEF(cdef: str):
-  cdef = cdef.replace("__declspec( dllexport )", "")
-  return cdef
-
-def load_ffi():
-  ffiPath = file_directory / "c_src/ffi.i"
-  ffi = cffi.FFI()
-  clean_ffi = (file_directory / "c_src/clean_ffi.i")
-  clean_ffi.write_text(cleanCDEF(ffiPath.read_text()))
-  ffi.cdef(clean_ffi.read_text())
-  return ffi
+def test_msvc_cffi_api_mode():
+  from build import build_pyd_msvc
+  build_pyd_msvc()
+  import c_src._example as example
+  mystruct = example.ffi.new("mystruct *")
+  print(mystruct)
 
 
-def load_dll_cffi_api_mode(ffi):
-  # This C code needs to make the declarated functions, types
-  # and globals available, so it is often just the "#include".
-  preamble = """
-  // passed to the real C compiler,
-  // contains implementation of things declared in cdef()
-  #include "example.h"   // the C header of the library
-  """
-  cffi_api_mode_c_file = (file_directory / ("c_src/cffi_example_dll.c")).absolute()
-  updated_c_source = make_c_source(
-    ffi, module_name="cffi_example_dll", preamble=preamble,
-    target_c_file=cffi_api_mode_c_file, verbose=True)
-
-  from build import build_cffi_api_mode
-  dll_file = build_cffi_api_mode(cffi_api_mode_c_file)
-  return dll_file
+def test_api_mode_asan():
+  from build import build_pyd_clang_asan
+  build_pyd_clang_asan()
+  # dumpbin(Path(r"c:\s\eklang\DevOps\clang\bin\LLVM-13.0.0-win64\lib\site-packages\lldb\_lldb.pyd"), exports=True)
+  dll_path_dir = repo / "c_src/pyd_clang_asan"
+  import pdb; pdb.set_trace()  # XXX (cesc)
+  os.add_dll_directory(str(dll_path_dir))
+  dumpbin (dll_path_dir / "cffi_wrap.pyd", exports=True)
+  import c_src.pyd_clang_asan.cffi_wrap as cffi_wrap
+  mystruct = cffi_wrap.ffi.new("mystruct *")
+  print(mystruct)
 
 
-def test_example(asan_cffi_api_mode=False, no_asan=False, asan=False):
-  ffi = load_ffi()
+def test_abi_mode_asan():
+    ffi = load_ffi()
+    build_dll_clang_asan()
+    dllPath = repo / "c_src/dll_clang_asan/example.dll"
+    print(f"dlopen {dllPath}")
+    C = ffi.dlopen(str(dllPath.absolute()))
 
-  dllPath = None
+    mystruct = ffi.new("mystruct *")
+    print(f'OK test_abi_mode asan')
 
-  if asan_cffi_api_mode:
-    dllPath = load_dll_cffi_api_mode(ffi)
 
-  if asan:
-    dllPath = file_directory / "c_src/asanexample.dll"
+def test_abi_mode():
+    ffi = load_ffi()
+    build_dll_clang()
+    dllPath = repo / "c_src/dll_clang/example.dll"
+    # dumpbin(dllPath, exports=True)
+    print(f"dlopen {dllPath}")
+    C = ffi.dlopen(str(dllPath.absolute()))
 
-  if no_asan:
-    dllPath = file_directory / "c_src/example.dll"
-
-  assert dllPath
-
-  print(f"dlopen {dllPath}")
-  C = ffi.dlopen(str(dllPath.absolute()))
-
-  mystruct = ffi.new("mystruct *")
+    mystruct = ffi.new("mystruct *")
+    print(f'OK test_abi_mode asan')
 
 
 class Mytest(unittest.TestCase):
@@ -70,13 +60,12 @@ class Mytest(unittest.TestCase):
 
 
 if __name__ == '__main__':
-  build()
 
-
-  # This seems ok, but not sure
-  test_example(no_asan=True)
-  test_example(asan=True)
-  # test_example(asan_cffi_api_mode=True)
-
-  # unittest explodes BOOM
+  # The following combination BOOM! :cry:
+  test_abi_mode()
+  test_abi_mode_asan()
   unittest.main()  # comment this line and AddressSanitizer will not complain
+
+  # test_api_mode_asan()
+
+
