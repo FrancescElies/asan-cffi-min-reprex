b32616fadc2a102103a709667b17d9c4d27ad91e
Francesc Elies <francesc.elies@mbbm-ast.com>
2021-11-15 09:12:16 +0100

    fix indents

--- test.py
+++ test.py
@@ -11,24 +11,24 @@ repo = file_path.parent
 
 
 def test_msvc_cffi_api_mode():
-  from build import build_pyd_msvc
-  build_pyd_msvc()
-  import c_src._example as example
-  mystruct = example.ffi.new("mystruct *")
-  print(mystruct)
+    from build import build_pyd_msvc
+    build_pyd_msvc()
+    import c_src._example as example
+    mystruct = example.ffi.new("mystruct *")
+    print(mystruct)
 
 
 def test_api_mode_asan():
-  from build import build_pyd_clang_asan
-  build_pyd_clang_asan()
-  # dumpbin(Path(r"c:\s\eklang\DevOps\clang\bin\LLVM-13.0.0-win64\lib\site-packages\lldb\_lldb.pyd"), exports=True)
-  dll_path_dir = repo / "c_src/pyd_clang_asan"
-  import pdb; pdb.set_trace()  # XXX (cesc)
-  os.add_dll_directory(str(dll_path_dir))
-  dumpbin (dll_path_dir / "cffi_wrap.pyd", exports=True)
-  import c_src.pyd_clang_asan.cffi_wrap as cffi_wrap
-  mystruct = cffi_wrap.ffi.new("mystruct *")
-  print(mystruct)
+    from build import build_pyd_clang_asan
+    build_pyd_clang_asan()
+    # dumpbin(Path(r"c:\s\eklang\DevOps\clang\bin\LLVM-13.0.0-win64\lib\site-packages\lldb\_lldb.pyd"), exports=True)
+    dll_path_dir = repo / "c_src/pyd_clang_asan"
+    import pdb; pdb.set_trace()  # XXX (cesc)
+    os.add_dll_directory(str(dll_path_dir))
+    dumpbin (dll_path_dir / "cffi_wrap.pyd", exports=True)
+    import c_src.pyd_clang_asan.cffi_wrap as cffi_wrap
+    mystruct = cffi_wrap.ffi.new("mystruct *")
+    print(f'OK API mode asan: {mystruct}')
 
 
 def test_abi_mode_asan():
@@ -39,7 +39,7 @@ def test_abi_mode_asan():
     C = ffi.dlopen(str(dllPath.absolute()))
 
     mystruct = ffi.new("mystruct *")
-    print(f'OK test_abi_mode asan')
+    print(f'OK ABI mode asan: {mystruct}')
 
 
 def test_abi_mode():
@@ -51,15 +51,13 @@ def test_abi_mode():
     C = ffi.dlopen(str(dllPath.absolute()))
 
     mystruct = ffi.new("mystruct *")
-    print(f'OK test_abi_mode asan')
+    print(f'OK ABI mode: {mystruct}')
 
 
 if __name__ == '__main__':
 
-  # The following combination BOOM! :cry:
-  test_abi_mode()
-  test_abi_mode_asan()
+    # The following combination BOOM! :cry:
+    test_abi_mode()
+    test_abi_mode_asan()
 
-  # test_api_mode_asan()
-
-
+    # test_api_mode_asan()
